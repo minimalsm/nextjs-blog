ef9851cbf56565c433f710331dd10a5c15c05865
Joshua <30259508@cityofglacol.ac.uk>
2021-11-27 21:38:47 +0000

    You pretty

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
@@ -20,18 +22,20 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hey! This is a sample website I'm using to learn Next.js. If you'd like to connect, follow me on <a href="https://twitter.com/JshuaETH">Twitter</a>.</p>
+        <p>Hey 👋 This is a sample website I'm using to learn Next.js. If you'd like to connect, follow me on <a href="https://twitter.com/JshuaETH">Twitter</a> ✌️</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
